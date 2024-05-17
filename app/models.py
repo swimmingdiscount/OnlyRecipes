9966558210d113fe97bb802c098a73064ac90aed
swimmingdiscount <23361098@student.uwa.edu.au>
2024-05-18 02:38:33 +0800

    class recipe added

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from app import db, login_manager
 
-# define the association table for many-to-many relationship between RecipeRequest and Ingredient
+# Define the association table for many-to-many relationship between RecipeRequest and Ingredient
 recipe_ingredients = db.Table('recipe_ingredients',
     db.Column('recipe_request_id', db.Integer, db.ForeignKey('recipe_request.id'), primary_key=True),
     db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
@@ -23,6 +23,7 @@ class User(db.Model, UserMixin):
     # Relationships
     recipe_requests = db.relationship('RecipeRequest', backref='author', lazy=True)
     replies = db.relationship('RecipeReply', backref='replier', lazy=True)
+    recipes = db.relationship('Recipe', backref='author', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -37,14 +38,28 @@ class RecipeRequest(db.Model):
     description = db.Column(db.Text, nullable=False)
     meal_type = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    ingredients = db.relationship('Ingredient', secondary=recipe_ingredients, lazy='subquery', backref=db.backref('recipes', lazy=True))
+    ingredients = db.relationship('Ingredient', secondary=recipe_ingredients, lazy='subquery', backref=db.backref('recipe_requests', lazy=True))
     
     # Foreign Key
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Recipe('{self.title}', '{self.created_at}')"
+
 
 class RecipeReply(db.Model):
     id = db.Column(db.Integer, primary_key=True)
